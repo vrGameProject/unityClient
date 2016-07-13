72bfcf4cdd81a20a4f46cf82ec54c3037a8faafd
ppang <yucaroll@naver.com>
2016-07-13 11:47:36 +0900

    마법 쏘기 ok

---
+++ Assets/Scripts/MagicSpellControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicSpellControl : MonoBehaviour {
+
+	public GameObject MagicIceBall;
+	public GvrViewer cardboard;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cardboard.Triggered) {
+			print ("triggered");
+
+			//transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z+shootSpeed * Time.deltaTime);
+			Instantiate(MagicIceBall, transform.position, transform.rotation);
+
+		}
+	}
+}
---
+++ Assets/Scripts/MagicSpellShot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicSpellShot : MonoBehaviour {
+
+	public GameObject camera;
+	Vector3 startPosition = new Vector3(0.0f, 0.5f, 0.0f);
+	float shootSpeed = 12.0f;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = Camera.main.transform.forward * 0.5f;
+		transform.position = startPosition;
+		//transform.position = startPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (direction);
+		//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + shootSpeed * Time.deltaTime);
+	}
+}
