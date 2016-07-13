a2e498d2d398c112dc7ef85a12edbffee0dc252d
ppang <yucaroll@naver.com>
2016-07-13 20:56:14 +0900

    마법 충돌 후 사라지기

--- Assets/Scripts/MagicSpellShot.cs
+++ Assets/Scripts/MagicSpellShot.cs
@@ -2,22 +2,33 @@
 using System.Collections;
 
 public class MagicSpellShot : MonoBehaviour {
-
-	public GameObject camera;
-	Vector3 startPosition = new Vector3(0.0f, 0.5f, 0.0f);
-	float shootSpeed = 12.0f;
+	
 	private Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
 		direction = Camera.main.transform.forward * 0.5f;
 		transform.position = Camera.main.transform.position;
-		//transform.position = startPosition;
+		transform.position = new Vector3 (transform.position.x, transform.position.y + 0.2f, +transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Destroy (gameObject, 3);
 		transform.Translate (direction);
-		//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + shootSpeed * Time.deltaTime);
+	}
+	/*
+	void onTriggerEnter(Collider col){
+		if (col.tag == "Enemy") {
+			Destroy (gameObject,0.1f);
+		}
+	}
+	*/
+
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "Enemy") {
+			print ("꿍해따니까");
+			Destroy (gameObject,0.05f);
+		}
 	}
 }
