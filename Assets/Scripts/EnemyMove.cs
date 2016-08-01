ececc5120756afe478cdfc5b8d95c180dfc2084c
ppang <yucaroll@naver.com>
2016-08-01 17:28:29 +0900

    적 랜덤하게 움직임

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour {
+
+	public GameObject enemy;
+	Transform myTrans;
+	float moveTime = 2.0f;
+
+	float randomX = 0.01f;
+	float randomZ = 0.01f;
+
+	bool updateLock = false;
+		
+	// Use this for initialization
+	void Start () {
+		myTrans = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (updateLock == false) {
+			
+			if (moveTime == 2.0f) {
+				randomX = Random.Range (-0.05f, 0.05f);
+				randomZ = Random.Range (-0.05f, 0.05f);
+			}
+
+			moveTime -= Time.deltaTime;
+
+			print ("time: " + moveTime);
+			print ("rx: " + randomX);
+			print ("rz: " + randomZ);
+
+			myTrans.position = new Vector3 (myTrans.position.x + randomX, myTrans.transform.position.y, myTrans.position.z + randomZ);
+
+			if (moveTime <= 0.0f) {
+				moveTime = 2.0f;
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.collider.tag == "MagicSpell") {
+			updateLock = true;
+		}
+	}
+}
--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -3,19 +3,36 @@ using System.Collections;
 
 public class EnemySpawn : MonoBehaviour {
 
-	public GameObject enemy;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+
 	float sponTime = 3.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		sponTime -= Time.deltaTime;
 		if (sponTime < 0) {
-			Instantiate (enemy, transform.position, Quaternion.identity);
+			int random = Random.Range (1, 4);
+			print ("random"+random);
+
+			switch (random) {
+			case 1:
+				Instantiate (enemy1, transform.position, Quaternion.identity);
+				break;
+			case 2:
+				Instantiate (enemy2, transform.position, Quaternion.identity);
+				break;
+			case 3:
+				Instantiate (enemy3, transform.position, Quaternion.identity);
+				break;
+			}
+
 			sponTime = 3.0f;
 		}
 	}
