a9a1c6c6d18c41c845041c60946ccc9dcbf3f143
gunhee cho <ckhee93@gmail.com>
2016-09-04 15:41:29 +0900

    ui 추가 handler 작업중

--- Assets/Script/Handler.cs
+++ Assets/Script/Handler.cs
@@ -1,8 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+public class Stage{
+	int numOfEnemy;
 
+	public int getNumofEnemy(){
+		return numOfEnemy;
+	}
+}
 public class Handler : MonoBehaviour {
-	private int numOfEnemy = 4;
+	//private int numOfEnemy = 4;
+
+	public GameObject enemy;
+	public Stage[] stage = new Stage[3];
 	//public AudioClip bgm;
 	// Use this for initialization
 	void Start () {
@@ -12,5 +21,11 @@ public class Handler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}public void spawn(int n){
+		float posX,posY=-5.0f;
+		for(int i =0;i<stage[n].getNumofEnemy();i++){
+			posX = Random.Range(-10.0f,10.0f);
+			Instantiate(enemy,new Vector2(posX,posY),transform.rotation);
+		}
 	}
 }
