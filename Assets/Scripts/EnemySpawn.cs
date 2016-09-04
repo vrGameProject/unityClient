1e38602cc085ffd0683041e1388575a3c35e1442
ppang <yucaroll@naver.com>
2016-09-04 16:26:29 +0900

    제한시간 설정. 시간 끝나면 적이 모두 사라짐!

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,6 +6,7 @@ public class EnemySpawn : MonoBehaviour {
 	public GameObject enemy1;
 	public GameObject enemy2;
 	public GameObject enemy3;
+	public static bool spawnLock = false;
 
 	float sponTime = 3.0f;
 
@@ -16,24 +17,27 @@ public class EnemySpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		sponTime -= Time.deltaTime;
-		if (sponTime < 0) {
-			int random = Random.Range (1, 4);
-			print ("random"+random);
 
-			switch (random) {
-			case 1:
-				Instantiate (enemy1, transform.position, Quaternion.identity);
-				break;
-			case 2:
-				Instantiate (enemy2, transform.position, Quaternion.identity);
-				break;
-			case 3:
-				Instantiate (enemy3, transform.position, Quaternion.identity);
-				break;
+		if (spawnLock == false) {
+			sponTime -= Time.deltaTime;
+			if (sponTime < 0) {
+				int random = Random.Range (1, 4);
+				print ("random" + random);
+
+				switch (random) {
+				case 1:
+					Instantiate (enemy1, transform.position, Quaternion.identity);
+					break;
+				case 2:
+					Instantiate (enemy2, transform.position, Quaternion.identity);
+					break;
+				case 3:
+					Instantiate (enemy3, transform.position, Quaternion.identity);
+					break;
+				}
+
+				sponTime = 3.0f;
 			}
-
-			sponTime = 3.0f;
 		}
 	}
 }
