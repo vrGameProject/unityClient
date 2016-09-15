23316aa85fbf9f8308a93a7420cbccd60a2ef621
gunhee cho <ckhee93@gmail.com>
2016-09-16 06:52:13 +0900

    make GameSelect Scene

---
+++ Assets/Script/GameSelect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSelect : MonoBehaviour {
+
+	private Vector3 center;
+	private string selectedGameBtn;
+	// Use this for initialization
+	void Start () {
+		center = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(center);
+		
+        RaycastHit hit;
+
+		if(Physics.Raycast(ray,out hit,10000f))
+		{
+			selectedGameBtn = hit.collider.name;
+		}
+
+		//손 인식 되면 게임 시작.
+	}
+}
