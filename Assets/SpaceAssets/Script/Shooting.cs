fa547ceae38ee6f25becb9dfd7c939c0e5d19049
gunhee cho <ckhee93@gmail.com>
2016-09-18 23:43:22 +0900

    풀링 구현

--- Assets/SpaceAssets/Script/Shooting.cs
+++ Assets/SpaceAssets/Script/Shooting.cs
@@ -12,10 +12,15 @@ public class Shooting : MonoBehaviour {
     private Vector3 center;
     private float count = 0;
     private float t;
+	private Queue bullets = new Queue ();
     // Use this for initialization
     void Start () {
         center = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2);
-        
+		for (int i = 0; i < 100; i++) {
+			GameObject tp = Instantiate(cube, leftGun.transform.position, transform.rotation) as GameObject;
+			tp.SetActive (false);
+			bullets.Enqueue (tp);
+		}
 	}
 	
 	// Update is called once per frame
@@ -27,7 +32,11 @@ public class Shooting : MonoBehaviour {
         if (t > 0.08f)
         {
             AudioSource.PlayClipAtPoint(shootSound,transform.position,0.05f);
-            GameObject tp = Instantiate(cube, leftGun.transform.position, transform.rotation) as GameObject;
+			GameObject bullet = bullets.Dequeue () as GameObject;
+			bullet.transform.position = leftGun.transform.position;
+			bullet.SetActive (true);
+
+			//GameObject tp = Instantiate(cube, leftGun.transform.position, transform.rotation) as GameObject;
             //GameObject tp2 = Instantiate(cube, rightGun.transform.position, transform.rotation) as GameObject;
             //tp.transform.Rotate(head.transform.eulerAngles);
             t = 0;
@@ -53,5 +62,9 @@ public class Shooting : MonoBehaviour {
         }*/
         
 	
+	}
+	public void bulletEnqueue(GameObject bullet){
+		bullets.Enqueue (bullet);
+		bullet.SetActive (false);
 	}
 }
--- Assets/SpaceAssets/Script/bullet.cs
+++ Assets/SpaceAssets/Script/bullet.cs
@@ -11,19 +11,23 @@ public class bullet : MonoBehaviour {
 	// Use this for initialization
     IEnumerator destroyBullet(){
         yield return new WaitForSeconds(2.0f);
-        Destroy(this.gameObject);
+		GameObject.Find ("Player").GetComponent<Shooting> ().bulletEnqueue (this.gameObject);
+        //Destroy(this.gameObject);
     }
 	void Start () {
         //AudioSource.PlayClipAtPoint(shootSound,transform.position);
         
         hand = GameObject.Find("LeftGun");
         direction = hand.transform.forward;
-        StartCoroutine(destroyBullet());
+        //StartCoroutine(destroyBullet());
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(direction*1.5f);
+		if(Vector3.Distance(hand.transform.position,transform.position) > 45.0f){
+			GameObject.Find ("Player").GetComponent<Shooting> ().bulletEnqueue (this.gameObject);
+		}
     }
     void OnCollisionEnter(Collision coll){
         //Debug.Log(coll.gameObject.name);
